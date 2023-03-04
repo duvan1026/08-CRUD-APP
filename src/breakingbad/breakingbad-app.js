df240c36c460ae8373911ea44f1d7aa501a4d8db
Duvan Castro <duvancastro1026@gmail.com>
2023-03-04 04:55:51 -0500

    FetchApi - request Http get in app

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,12 +1,28 @@
 
 
 /**
+ * @returns {Promese<Object>} quote information
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json(); // todo promesa que retorna la data del body
+
+    console.log(data);
+    return data;
+}
+
+/**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = ( element ) => { //Todo se colocala la primera en mayuscula porque es el componente principal de mi aplicación.
+export const BreakingbadApp = async( element ) => { //Todo se colocala la primera en mayuscula porque es el componente principal de mi aplicación.
 
     document.querySelector('#app-title').innerHTML = 'Breaking App';
     element.innerHTML = 'Loading...'
+
+    const quote = await fetchQuote();
+    element.innerHTML = 'Tenemos data!!!';
+
 
 }
