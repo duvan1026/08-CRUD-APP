eb1ba496e4174cdbc546dabcb5d8f7f20636662c
Duvan Castro <duvancastro1026@gmail.com>
2023-03-05 07:07:06 -0500

    create structure 'store' page module

---
+++ src/users/store/users-store.js
@@ -0,0 +1,31 @@
+
+
+const state = {
+    currentPage: 0,
+    user: [],
+}
+
+
+const loadNextPage = async() => {
+    throw new Error ('No implementado');
+}
+
+
+const loadPreviosPage = async() => {
+    throw new Error ('No implementado');
+
+}
+
+// Todo: implementar
+/**
+ * se implementa cuando un usuario cambie
+ */
+const onUserChanged = () => {
+    throw new Error ('No implementado');
+
+}
+
+const reloadPage = async() => {
+    throw new Error ('No implementado');
+
+}
