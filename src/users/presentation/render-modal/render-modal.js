1c1faefb5c91fc3edba977bcbabb4076b268335e
Duvan Castro <duvancastro1026@gmail.com>
2023-03-07 04:06:56 -0500

    basic first implemented for modal in application

---
+++ src/users/presentation/render-modal/render-modal.js
@@ -0,0 +1,22 @@
+import modalHtml from "./render-modal.html?raw";
+
+
+let modal;
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderModal = ( element ) => {
+
+    if ( modal ) return;
+
+    modal = document.createElement('div');
+    modal.innerHTML = modalHtml;
+    modal.className = 'modal-container  hide-modal'; // Todo: clases para ocultar el modal
+
+    element.append( modal );
+
+
+}
