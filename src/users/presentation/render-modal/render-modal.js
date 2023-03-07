241a911c316b29ddd4373adf08e77d6cbe3ef0d1
Duvan Castro <duvancastro1026@gmail.com>
2023-03-07 04:45:43 -0500

    prevent form from propagating

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -2,7 +2,7 @@ import modalHtml from "./render-modal.html?raw";
 import "./render-modal.css";
 
 
-let modal;
+let modal, form;
 
 
 //Todo: cargar usuario por id
@@ -30,12 +30,21 @@ export const renderModal = ( element ) => {
     modal = document.createElement('div');
     modal.innerHTML = modalHtml;
     modal.className = 'modal-container  hide-modal'; // Todo: clases para ocultar el modal
+    form = modal.querySelector('form'); // Obtiene referencia al formulario
+
 
     modal.addEventListener( 'click', (event) => {
         
         if (event.target.className !== 'modal-container') return; // todo: pregunta si el usuario a dado click dentro del modal, si no lo es, no retorna y continua.
 
         hideModal();// Todo: esconde el modal
+    });
+
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Evita que el form se comporte normalmente( propagacion del mismo ) y envie los datos al backend
+
+        console.log('Formulario enviado');
     });
 
 
