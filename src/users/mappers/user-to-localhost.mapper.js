a63e8138896f4b26717b0cdf29f61954ba797e4f
Duvan Castro <duvancastro1026@gmail.com>
2023-03-08 04:16:05 -0500

    update documentación file

--- src/users/mappers/user-to-localhost.mapper.js
+++ src/users/mappers/user-to-localhost.mapper.js
@@ -3,7 +3,9 @@ import { User } from "../models/user";
 
 
 /**
- * 
+ * Recibe nuestro objeto User en el cual se esta trabajando
+ * luego se convierte la variable del dao a su nombre correspondiente en el back-end
+ * así solucionando posibles conflictos entre el back-end y el fornt-end
  * @param {User} user 
  */
 export const userModelToLocalHost = ( user ) => {
